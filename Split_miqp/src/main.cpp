f8417509a4795ae9dbcd98ef06eec1c423503000
Keiji Kimura <kgeewimp@gmai.com>
2018-05-01 23:37:16 +0900

    Make basic files

--- Split_miqp/src/main.cpp
+++ Split_miqp/src/main.cpp
@@ -9,8 +9,6 @@
 #include <iomanip>
 
 #include "read_data.h"
-//#include "vector.h"
-//#include "qpsolver.h"
 #include "svpsolver.h"
 
 #define debug 1
@@ -19,124 +17,35 @@ using namespace std;
 
 int main( int argc, char** argv){
 
-	cout << fixed << setprecision(3);
-	if( argc != 3 ){
-		cerr << "Error: commandline arguments" << endl;
-		return -1;
-	}
-	int	nthreads = atoi( argv[argc-1] );
-	assert( nthreads > 0 );
+   cout << fixed << setprecision(3);
 
-	int		m;
+   if( argc != 2 )
+   {
+      cerr << "Error: commandline arguments" << endl;
+      return -1;
+   }
 
-	ReadDim( argv[1], &m);
+   int m;
 
-	assert( m>0 );
+   ReadDim( argv[1], &m);
 
-	double	*B_ = NULL;		// [m*m], Colmajor
-	B_	=	new double[m*m];
+   assert( m > 0 );
 
-	ReadData( argv[1], m, B_);
+   double *B_ = nullptr;    // [m*m], Colmajor
+   B_ = new double[ m * m ];
 
-	SVPsolver	svps;
+   ReadData( argv[1], m, B_);
 
-	svps.create_probdata( m, B_);
-	svps.create_sch( m, B_);
-	svps.set_num_thread( nthreads );
-	svps.find_min_column();
-	svps.compute_bounds();
+   SVPsolver   svps;
+   auto nthreads = 1;
 
-	bool run;
-	if( nthreads == 1 ){
-		run = svps.solve(false, true, 0.0, TIMELIMIT);
-	}else{
-		run = svps.p_solve();
-	}
+   svps.create_probdata( m, B_ );
+   svps.create_sch( m, B_ );
+   svps.set_num_thread( nthreads );
+   svps.find_min_column();
+   svps.compute_bounds();
 
-	if( !run ){
-		cout << "could not solve .." << endl;
-		return 0;
-	}
+   delete[] B_;
 
-	svps.disp_bestsol();
-
-//
-//
-//	QPsolver	 qps;
-//	qps.set_dim( m );
-//	qps.set_obj_quad( m, Q);
-//
-//	double	*A;
-//	A = new double[m];
-//	for(int i=0; i<m; i++){
-//		if( i < 10 ){
-//			A[i] = -1.0;
-//		}else{
-//			A[i] = 0.0;
-//		}
-//	}
-//	double	*b;
-//	b = new double[1];
-//	b[0] = -1.0;
-//	//qps.set_ineq( m, 1, A, b);
-//
-//	double	*C;
-//	C = new double[m];
-//	for(int i=0; i<m; i++){
-//		if( (10<=i) && (i<20) ){
-//			C[i] = 1.0;
-//		}else{
-//			C[i] = 0.0;
-//		}
-//	}
-//	double 	*d;
-//	d = new double[1];
-//	d[0] = 1.0;
-//	//qps.set_equ( m, 1, C, d);
-//
-//	l[0] = 1.0;
-//	u[1] = -1.0;
-//	qps.set_lb( m, l);
-//	qps.set_ub( m, u);
-//
-//	qps.disp_prob();
-//	
-//	double	*warm;
-//	warm = new double[m];
-//
-//	warm[0] = 3.0;
-//	warm[1] = -3.0;
-//	for(int i=2; i<=10; i++){
-//		warm[i] = 1.0;
-//	}
-//	for(int i=11; i<=19; i++){
-//		warm[i] = 0.0;
-//	}
-//	for(int i=20; i<m; i++){
-//		warm[i] = 1.0;
-//	}
-//
-//	for(int i=30; i<35; i++){
-//		warm[i] = l[i];
-//	}
-//	for(int i=35; i<m; i++){
-//		warm[i] = u[i];
-//	}
-//
-//	qps.set_warm( m, warm);
-//	qps.solve();
-//
-	delete[] B_;
-//	delete[] nrm;
-//	delete[] l;
-//	delete[] u;
-//
-//	delete[] Q;
-//	delete[] A;
-//	delete[] b;
-//	delete[] C;
-//	delete[] d;
-//	delete[] warm;
-
-	return 0;
+   return 0;
 }
