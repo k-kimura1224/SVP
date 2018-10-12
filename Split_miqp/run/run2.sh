f76085477552ca998d48faba8bd9fd9031903fa9
Keiji <kgeewimp@gmail.com>
2018-10-12 10:11:59 +0900

    add Split_miqp/result/seed0_4

---
+++ Split_miqp/run/run2.sh
@@ -0,0 +1,43 @@
+#/bin/sh
+#chmod u+x gen.sh
+
+run()
+{
+   file=$1
+
+   buf=${1##*/}
+   name=${buf%.*}
+
+   thread=$2
+   time=$3
+
+	export OMP_NUM_THREADS=1
+   echo "../../bin/SVPSOLVER.opt -f $file -p $thread -t $time -q > ${name}.log"
+   ../../bin/SVPSOLVER.opt -f $file -p $thread -t $time -q > ${name}.log
+}
+
+dirname=$1
+thread=$2
+time=$3
+
+#for file in `ls ${dirname}/*.dat`
+#do
+#   run $file $thread $3
+#done
+#for file in `ls ../../../link/*/*.dat`
+#do
+#   run $file 32 86400
+#done
+
+for i in 40 43 46 49
+do
+	for j in 0 1 2 3 4
+	do
+		file=../../../link/B20_${i}s/BKZ20-SVP_n${i}_seed${j}.dat
+   	run $file 32 86400
+	done
+done
+
+
+
+
